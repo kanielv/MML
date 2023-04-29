1dc09c621d0ae6eb5b8f75464fc4adb19b933d04
kanielv <kanielvicencio@gmail.com>
2023-04-29 00:32:20 -0400

    added data visulizations for feature plots

--- Assignment3/src/utils/data_visulization.py
+++ Assignment3/src/utils/data_visulization.py
@@ -36,7 +36,26 @@ def plot_frquency(y, sr, name):
     plt.show()
 
 def plot_mfcc(y, sr, name):
-    
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
+    librosa.display.specshow(mfccs, sr=sr, x_axis='time', y_axis='log')
+    plt.title(name)
+    plt.show()
+
+def plot_chromagram(y, sr, name):
+    chromagram = librosa.feature.chroma_stft(y=y, sr=sr)
+    librosa.display.specshow(chromagram, sr=sr, x_axis='time', y_axis='log')
+    plt.title(name)
+    plt.show()
+
+def plot_mel_spectrogram(y, sr, name):
+    mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=12)
+    librosa.display.specshow(mel_spectrogram, sr=sr, x_axis='time', y_axis='log')
+    plt.title(name)
+    plt.show()
+
+
+
+
 
 
 
