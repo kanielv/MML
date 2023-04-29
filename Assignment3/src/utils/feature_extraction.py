f3a70fd2800bf26e2b939f4038d4b9166532ea63
kanielv <kanielvicencio@gmail.com>
2023-04-29 01:27:43 -0400

    added audio display, began feature extraction for training set

--- Assignment3/src/utils/feature_extraction.py
+++ Assignment3/src/utils/feature_extraction.py
@@ -18,31 +18,36 @@ def get_waveform(fpath):
 # feature extraction: mel-freq cepstral coefficients 
 def get_mfccs(y, sr):
     df_mfccs = pd.DataFrame()
-
     mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
-    for n_mfcc in range(len(mfccs)):
-        df_mfccs['MFCC_%d'%(n_mfcc+1)] = mfccs.T[n_mfcc]
+    norm = mfccs / np.linalg.norm(mfccs)
 
-    return df_mfccs, mfccs
+    for n_mfcc in range(len(norm)):
+        df_mfccs['MFCC_%d'%(n_mfcc+1)] = norm.T[n_mfcc]
+
+    return df_mfccs, norm
 
 # feature extraction: mel spectogram
 
 def get_melspectrogram(y, sr):
     df_melspectrogram = pd.DataFrame()
     mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=12)
-    for n_mel in range(len(mel_spectrogram)):
-        df_melspectrogram['Mel_Spectogram_%d'%(n_mel+1)] = mel_spectrogram.T[n_mel]
+    norm = mel_spectrogram / np.linalg.norm(mel_spectrogram)
 
-    return df_melspectrogram, mel_spectrogram
+    for n_mel in range(len(norm)):
+        df_melspectrogram['Mel_Spectogram_%d'%(n_mel+1)] = norm.T[n_mel]
+
+    return df_melspectrogram, norm
 
 # feature extraction: chroma
 def get_chromagram(y, sr):
     df_chroma = pd.DataFrame()
     chromagram = librosa.feature.chroma_stft(y=y, sr=sr)
-    for n_chroma in range(len(chromagram)):
-        df_chroma['Chroma_%d'%(n_chroma+1)] = chromagram.T[n_chroma]
+    norm = chromagram / np.linalg.norm(chromagram)
 
-    return df_chroma, chromagram
+    for n_chroma in range(len(norm)):
+        df_chroma['Chroma_%d'%(n_chroma+1)] = norm.T[n_chroma]
+
+    return df_chroma, norm
 
 # generate a feature matix with the 3 features 
 def generate_feature_matrix(df_mfccs, df_chroma, df_melspectrogram):
