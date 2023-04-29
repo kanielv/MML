de5e2cdde1e8c74ad3b2b00f9339135416b0a11e
kanielv <kanielvicencio@gmail.com>
2023-04-28 23:36:15 -0400

    implemented get_mfccs for feature extraction

--- Assignment3/src/utils/data_visulization.py
+++ Assignment3/src/utils/data_visulization.py
@@ -7,18 +7,33 @@ Author: Kaniel Vicencio
 
 import matplotlib.pyplot as plt
 import numpy as np
-import wave 
-
-def get_waveform(fpath):
-    wav_obj = wave.open(fpath, 'r')
-    return wav_obj
 
 import librosa 
 import librosa.display
-def plot_waveform(wf_path, name):
+def plot_waveform(y, sr, name):
 
-    y, sr = librosa.load(wf_path, duration=10)
     librosa.display.waveshow(y, sr=sr)
     plt.title(name)
+    plt.xlabel('Time / second')
+    plt.ylabel('Amplitude')
+
+    plt.show()
+
+def plot_frquency(y, sr, name):
+    
+    k = np.arange(len(y))
+    T = len(y)/sr
+    freq = k/T
+
+    DATA_0 = np.fft.fft(y)
+    abs_DATA_0 = abs(DATA_0)
+
+    plt.title(name)
+    plt.plot(freq, abs_DATA_0)
+    plt.xlabel('Freq / Hz')
+    plt.ylabel('Amplitude / dB')
+    plt.xlim([0, 1000])
+    plt.show()
+
 
 
--- Assignment3/src/utils/feature_extraction.py
+++ Assignment3/src/utils/feature_extraction.py
@@ -3,3 +3,25 @@ This file contains functions for feature extractions
 Author: Kaniel Vicencio
 '''
 
+import librosa
+import numpy as np
+import pandas as pd
+
+# Waveform
+def get_waveform(fpath): 
+    signal, sample_rate = librosa.load(fpath)
+    return signal, sample_rate
+
+# feature extraction: mel-freq cepstral coefficients 
+def get_mfccs(y, sr):
+    df_mfccs = pd.DataFrame()
+
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
+    for n_mfcc in range(len(mfccs)):
+        df_mfccs['MFCC_%d'%(n_mfcc+1)] = mfccs.T[n_mfcc]
+    
+    # visualize mfccs (for debugging)
+    # print(df_mfccs.head())
+    # librosa.display.specshow(mfccs, sr=sr, x_axis='time', y_axis='log')
+
+# feature extraction: mel spectogram
